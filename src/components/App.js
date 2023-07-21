cdb6e5aeb519d5da717407974131f7e8e561a288
Nigel <nigel.oketch@student.moringaschool.com>
2023-07-21 03:29:11 +0300

    completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,11 @@
-import React from "react";
-import blogData from "../data/blog";
+import "@testing-library/jest-dom";
+import { render } from "@testing-library/react";
+import App from "../components/App";
 
-console.log(blogData);
-
-function App() {
-  return (
-    <div className="App">
-      You're on your own from here! Follow the deliverables; test things out in
-      the browser as you write your code; and good luck!
-    </div>
-  );
-}
-
-export default App;
+test("renders the correct child components", () => {
+  const { container } = render(<App />);
+  expect(container.querySelector(".App")).toBeInTheDocument();
+  expect(container.querySelector(".App header")).toBeInTheDocument();
+  expect(container.querySelector(".App aside")).toBeInTheDocument();
+  expect(container.querySelector(".App main")).toBeInTheDocument();
+});
